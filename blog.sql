3702cf9447775e234e0b270cf889af31393d245a
BadMaker <107988902+Bad-maker@users.noreply.github.com>
2024-07-04 17:11:00 +0800

    Update blog.sql

--- blog.sql
+++ blog.sql
@@ -3,11 +3,11 @@ CREATE DATABASE blog;
 USE blog;
 
 -- 用户表
-CREATE TABLE users (
+CREATE TABLE user (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(20) NOT NULL,
-    PASSWORD VARCHAR(20) NOT NULL,
-    ROLE VARCHAR(10) NOT NULL,
+    PASSWORD VARCHAR(100) NOT NULL,
+    ROLE VARCHAR(10),
     provider VARCHAR(20),
     provider_id VARCHAR(100),
     email VARCHAR(40) UNIQUE,
@@ -19,14 +19,14 @@ CREATE TABLE users (
 
 
 -- 标签表
-CREATE TABLE tags (
+CREATE TABLE tag (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     NAME VARCHAR(50) NOT NULL UNIQUE
 );
 
 
 -- 分类表
-CREATE TABLE categories (
+CREATE TABLE categorie (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     NAME VARCHAR(50) NOT NULL UNIQUE
 );
@@ -34,7 +34,7 @@ CREATE TABLE categories (
 
 
 -- 文章表
-CREATE TABLE articles (
+CREATE TABLE article (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     content TEXT NOT NULL,
@@ -47,7 +47,7 @@ CREATE TABLE articles (
 
 
 -- 评论表
-CREATE TABLE comments (
+CREATE TABLE comment (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     content TEXT NOT NULL,
     created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -59,7 +59,7 @@ CREATE TABLE comments (
 
 
 -- 文章标签关联表
-CREATE TABLE article_tags (
+CREATE TABLE article_tag (
     article_id BIGINT,
     tag_id BIGINT,
     FOREIGN KEY (article_id) REFERENCES articles(id),
@@ -70,10 +70,10 @@ CREATE TABLE article_tags (
 
 
 
--- users 表存储用户信息，包括用户名、密码和角色。
--- articles 表存储博客文章信息，包括标题、内容、创建日期、作者ID和分类ID。
--- comments 表存储评论信息，包括内容、创建日期、作者ID和文章ID。
--- tags 表存储标签信息，包括标签名。
+-- user 表存储用户信息，包括用户名、密码和角色。
+-- article 表存储博客文章信息，包括标题、内容、创建日期、作者ID和分类ID。
+-- comment 表存储评论信息，包括内容、创建日期、作者ID和文章ID。
+-- tag 表存储标签信息，包括标签名。
 -- categories 表存储分类信息，包括分类名。
 -- article_tags 表存储文章与标签的多对多关系。
 
